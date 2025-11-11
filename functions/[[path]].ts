dd61fc086225f358da1425879bfcf416ccd686e2
eltphd <erica@measurementally.com>
2025-11-10 19:41:19 -0500

    Fetch marketing directory instead of index for root route

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -2,7 +2,7 @@ export const onRequest = async ({ request, env }: { request: Request; env: any }
   const url = new URL(request.url);
 
   if (url.pathname === "/" || url.pathname === "/index.html") {
-    const target = new URL("/marketing/index.html", url.origin);
+    const target = new URL("/marketing/", url.origin);
     return env.ASSETS.fetch(new Request(target.toString(), request));
   }
 
