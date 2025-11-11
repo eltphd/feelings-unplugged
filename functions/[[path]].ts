060fdc360a706b5e7f74072fc99126a48c3c2fec
eltphd <erica@measurementally.com>
2025-11-10 19:39:38 -0500

    Handle root routing via catch-all Pages Function

---
+++ functions/[[path]].ts
@@ -0,0 +1,11 @@
+export const onRequest = async ({ request, env }: { request: Request; env: any }) => {
+  const url = new URL(request.url);
+
+  if (url.pathname === "/" || url.pathname === "/index.html") {
+    const target = new URL("/marketing/index.html", url.origin);
+    return env.ASSETS.fetch(new Request(target.toString(), request));
+  }
+
+  return env.ASSETS.fetch(request);
+};
+
